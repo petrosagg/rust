79372e95c934e5b73f79840ae519d7d4b412f7bf
Petros Angelatos <petrosagg@gmail.com>
2025-04-10 11:09:31 +0300

    reproduce double free
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

--- library/std/tests/sync/mpmc.rs
+++ library/std/tests/sync/mpmc.rs
@@ -17,6 +17,24 @@ fn smoke() {
 }
 
 #[test]
+fn gh_139553() {
+    let (s1, r) = channel::<u64>();
+    let s2 = s1.clone();
+    // This thread will sleep for 2000ms at the critical moment
+    let t1 = thread::spawn(move || assert!(s1.send(42).is_err()));
+    // Give some time for thread 1 to reach the critical state
+    thread::sleep(Duration::from_millis(100));
+    // Send another value which see the tail is not null and will just advance the tail offset to 1
+    // and write the value.
+    s2.send(42).unwrap();
+    // Now drop the receiver which will attempt to drop a message by reading it since head != tail
+    // but head is still a null pointer because the thread t1 is still preempted leading to a
+    // segfault.
+    drop(r);
+    t1.join().unwrap();
+}
+
+#[test]
 fn drop_full() {
     let (tx, _rx) = channel::<Box<isize>>();
     tx.send(Box::new(1)).unwrap();
